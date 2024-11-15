e909d9e2af1c80a8bba11d949eda7b316115cd45
raffael.gyr <raffael.gyr@cubera.ch>
2024-11-15 14:30:36 +0100

    make DrawableButton class an object directly

--- progressbutton/src/main/java/com/github/razir/progressbutton/DrawableButton.kt
+++ progressbutton/src/main/java/com/github/razir/progressbutton/DrawableButton.kt
@@ -1,25 +1,23 @@
 package com.github.razir.progressbutton
 
-class DrawableButton {
-    companion object {
+object DrawableButton {
 
-        /**
-         *   Show drawable on the left of the text
-         */
-        const val GRAVITY_TEXT_START = 0
-        /**
-         *   Show drawable on the right of the text
-         */
-        const val GRAVITY_TEXT_END = 1
-        /**
-         *   Show drawable on the center. Use only if your text is null or empty
-         */
-        const val GRAVITY_CENTER = 2
+    /**
+     *   Show drawable on the left of the text
+     */
+    const val GRAVITY_TEXT_START = 0
+    /**
+     *   Show drawable on the right of the text
+     */
+    const val GRAVITY_TEXT_END = 1
+    /**
+     *   Show drawable on the center. Use only if your text is null or empty
+     */
+    const val GRAVITY_CENTER = 2
 
 
-        /**
-         *  defines the default value for a param if it possible
-         */
-        const val DEFAULT = -1
-    }
+    /**
+     *  defines the default value for a param if it possible
+     */
+    const val DEFAULT = -1
 }
